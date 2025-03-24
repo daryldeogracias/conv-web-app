13fd8a289913b4036c8dbeea364fde824b630944
daryldeogracias <dryldeogracias@gmail.com>
2025-03-25 00:55:01 +0800

    chore: new logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Mode from "./Mode";
+import Bag from "@/public/Bag";
 
 const Navbar = () => {
   return (
@@ -9,7 +10,7 @@ const Navbar = () => {
       <div className="fixed top-0 left-0 w-full border-b border-stone-800 z-50 bg-white dark:bg-black">
         <div className=" bg-linear-to-bl from-violet-400 to-violet-500 p-3"></div>
         <div className="flex items-center justify-between max-w-[1200px] mx-auto py-4">
-          <p className="font-bold text-yellow-400 text-lg">X//</p>
+          <p className="font-medium text-green-400 text-lg">X//</p>
           <div className="flex items-center gap-6 text-black dark:text-white">
             <div className="cursor-pointer hover:underline underline-offset-2">
               Reviews
@@ -20,8 +21,9 @@ const Navbar = () => {
             <div className="cursor-pointer hover:underline underline-offset-2">
               Benefits
             </div>
-            <div className="cursor-pointer hover:underline underline-offset-2">
-              Checkout
+            <div className="flex items-center gap-2 cursor-pointer hover:underline underline-offset-2">
+              <div>Checkout</div>
+              <Bag />
             </div>
           </div>
           <Mode />
