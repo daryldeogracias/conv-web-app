5c6cb3641a6da5935b505e0866cb727a46f90608
daryldeogracias <dryldeogracias@gmail.com>
2025-03-26 00:33:56 +0800

    wip: draft mobile landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,38 @@
 import React from "react";
 import Image from "next/image";
 import Truck from "@/public/Truck";
+import Bag from "@/public/Bag";
 
 const Home = () => {
   return (
     <div className="h-full">
-      <section className="grid grid-cols-2 items-center mt-12 lg:mx-6 xl:mx-0">
+      <section className="flex flex-col-reverse  lg:flex-none lg:grid lg:grid-cols-2 items-center mt-12 mx-4 sm:mx-5 lg:mx-6 xl:mx-0">
         <div>
           <div className="p-1 px-2 mt-6 text-center text-xs text-violet-800 font-medium  bg-violet-800/10 dark:text-violet-400 dark:bg-violet-500/30 mb-2 max-w-max rounded-full">
             🩺 clinically reviewed formula
           </div>
-          <h1 className="text-4xl font-bold leading-12">
+          <h1 className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-bold sm:leading-8 lg:leading-10 xl:leading-12">
             Sleep deeper and wake up refreshed with{" "}
             <span className="text-violet-500">Megneta 8-in-1</span>.{" "}
             <span className="text-violet-500">Natural</span> , non-groggy
             support for better rest—starting tonight!
           </h1>
-          <div className="flex flex-col w-full justify-start mt-8">
-            <div className="flex items-center gap-2 text-lg font-medium">
+          <div className="flex w-full px-2 py-2 gap-2 justify-center mt-3 lg:hidden bg-violet-500 text-white rounded-md font-semibold">
+            <p>Checkout</p>
+            <Bag />
+            <p>{"->"}</p>
+          </div>
+          <div className="flex flex-col w-full justify-start mt-6 lg:mt-8">
+            <div className="flex items-center gap-2 text-xs sm:text-lg lg:text-lg font-medium">
               <Truck width={40} height={40} />
-              <p className="cursor-pointer hover:underline underline-offset-1">
-                Free shippping guaranteed{" "}
+              <p className="flex flex-col sm:flex-row gap-1 lg:gap-1 w-full cursor-pointer hover:underline underline-offset-1">
+                Free shippping guaranteed
                 <span className="text-green-500">
                   {"(only 12 left for this offer)"}
                 </span>{" "}
-                {"->"}
               </p>
             </div>
-            <div className="relative h-6 w-62 ml-8">
+            <div className="relative h-6 w-62 ml-8 mt-2 lg:mt-0">
               <Image
                 src="/payments.png"
                 alt="supported payments method"
@@ -82,7 +87,7 @@ const Home = () => {
           </div>
         </div>
         <div className="flex justify-end">
-          <div className="h-120 w-120 relative">
+          <div className="h-80 w-80 lg:h-100 lg:w-100 xl:h-120 xl:w-120 relative">
             <Image
               src="/product.png"
               alt="Megneta 8-in-1 Sleep Supplement"
