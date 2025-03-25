3678be0f6e486508bf4d0f045f6917e8b77ee23b
daryldeogracias <dryldeogracias@gmail.com>
2025-03-26 00:50:02 +0800

    wip: clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Truck from "@/public/Truck";
 import Bag from "@/public/Bag";
+import { useTheme } from "next-themes";
 
 const Home = () => {
+  const { theme } = useTheme();
   return (
     <div className="h-full">
       <section className="flex flex-col-reverse  lg:flex-none lg:grid lg:grid-cols-2 items-center mt-12 mx-4 sm:mx-5 lg:mx-6 xl:mx-0">
         <div>
-          <div className="p-1 px-2 mt-6 text-center text-xs text-violet-800 font-medium  bg-violet-800/10 dark:text-violet-400 dark:bg-violet-500/30 mb-2 max-w-max rounded-full">
+          <div
+            className={`p-1 px-2 mt-6 text-center text-xs font-medium mb-2 max-w-max rounded-full
+             ${
+               theme === "light"
+                 ? "text-violet-800 bg-violet-800/10"
+                 : "text-violet-400 dark:bg-violet-500/30 "
+             }
+            `}
+          >
             🩺 clinically reviewed formula
           </div>
           <h1 className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-bold sm:leading-8 lg:leading-10 xl:leading-12">
@@ -23,16 +35,17 @@ const Home = () => {
             <p>{"->"}</p>
           </div>
           <div className="flex flex-col w-full justify-start mt-6 lg:mt-8">
-            <div className="flex items-center gap-2 text-xs sm:text-lg lg:text-lg font-medium">
+            <div className="flex items-center gap-3 lg:gap-2 text-xs sm:text-lg lg:text-lg font-medium">
               <Truck width={40} height={40} />
-              <p className="flex flex-col sm:flex-row gap-1 lg:gap-1 w-full cursor-pointer hover:underline underline-offset-1">
-                Free shippping guaranteed
-                <span className="text-green-500">
-                  {"(only 12 left for this offer)"}
-                </span>{" "}
+              <p className="w-full cursor-pointer hover:underline underline-offset-1">
+                Free shipping guaranteed
+                <span className="text-green-500 ml-1">
+                  (only 12 left for this offer)
+                </span>
+                <span className="hidden lg:inline ml-1">{"->"}</span>
               </p>
             </div>
-            <div className="relative h-6 w-62 ml-8 mt-2 lg:mt-0">
+            <div className="relative h-6 w-62 ml-8 lg:mt-0">
               <Image
                 src="/payments.png"
                 alt="supported payments method"
@@ -41,11 +54,11 @@ const Home = () => {
                 className="object-contain flex justify-start"
               />
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col w-full items-center lg:items-start">
               <div className="p-1 px-2 mt-6 ml-2 text-start text-[10px]">
                 certified product by:
               </div>
-              <div className="flex w-full items-center">
+              <div className="flex w-full items-center justify-center lg:justify-start">
                 <div className="relative h-12 w-20">
                   <Image
                     src="/who.png"
